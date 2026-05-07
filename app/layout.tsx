3136cfbe50abfd877663a55b72479828efd5c786
NothingHereDontCheck <jraine00@gmail.com>
2026-05-07 17:09:02 -0400

    Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { DM_Serif_Display, Outfit, DM_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const dmSerifDisplay = DM_Serif_Display({
@@ -51,7 +52,28 @@ export default function RootLayout({
       lang="en"
       className={`${dmSerifDisplay.variable} ${outfit.variable} ${dmMono.variable}`}
     >
-      <body>{children}</body>
+      <Script
+        id="gtm"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WQ9J3453');`,
+        }}
+      />
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WQ9J3453"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   )
 }
